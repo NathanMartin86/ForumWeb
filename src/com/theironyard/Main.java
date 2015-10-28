cea1269414c8836f8f908763d3c484a50f28f55e
macbookair <nj.martin123@gmail.com>
2015-10-28 11:56:15 -0400

    Add Login

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,6 +1,7 @@
 package com.theironyard;
 
 import spark.ModelAndView;
+import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
 
@@ -13,13 +14,70 @@ public class Main {
         HashMap<String, User> users = new HashMap<>();
         ArrayList<Message> messages = new ArrayList<>();
 
+        addTestUsers(users);
+        addTestMessages(messages);
+
         Spark.get(
                 "/",
                 ((request, response) ->{
+                    Session session = request.session();
+                    String username = session.attribute("username");
+
+                    ArrayList<Message> threads = new ArrayList<>();
+
+                    for (Message message : messages) {
+                        if (message.replyId == -1) {
+                            threads.add(message);
+                        }
+                    }
+
                     HashMap m = new HashMap();
+                    m.put("username" ,username);
+                    m.put("threads", threads);
                     return new ModelAndView(m, "threads.html");
                 }),
                 new MustacheTemplateEngine()
         );
+        Spark.post(
+                "/login",
+                ((request, response) -> {
+                    String username = request.queryParams("username");
+                    String password = request.queryParams("password");
+                    if (username.isEmpty() || password.isEmpty()){
+                        Spark.halt(403);
+                    }
+
+                    User user = users.get(username);
+                    if (user == null){
+                        user = new User();
+                        user.password = password;
+                        users.put(username, user);
+                    }
+                    else if (!password.equals(user.password)){
+                        Spark.halt(403);
+                    }
+
+                    Session session = request.session();
+                    session.attribute("username", username);
+
+                    response.redirect("/");
+                    return "";
+                })
+        );
+    }
+
+
+    static void addTestUsers(HashMap<String,User> users){
+        users.put("Alice", new User());
+        users.put("Bob", new User());
+        users.put("Charlie", new User());
+
+    }
+    static void addTestMessages(ArrayList<Message> messages){
+        messages.add(new Message(0,-1, "Alice","This is a thread!"));
+        messages.add(new Message(1,-1, "Bob","This is a thread!"));
+        messages.add(new Message(2, 0, "Charlie","Cool Thread Alice!"));
+        messages.add(new Message(3, 2, "Alice","Thanks!"));
+
     }
 }
